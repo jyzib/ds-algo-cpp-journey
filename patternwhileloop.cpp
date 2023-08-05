d86fdc420fa9d5a9553bf3bf3646fab6230f0089
jyzib <jazibzaidi02@gmail.com>
2023-08-05 22:49:13 +0530

    pattren_question

---
+++ patternwhileloop.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout<<"Enter number - ";
+    cin>>n;
+    int x = 0;
+    int p = 0;
+    while(x<n){
+      p = 0;
+     while(p<n){
+        cout<<"*";
+        p++;
+ 
+    };
+    cout<<"\n";
+  x++;
+ 
+    };
+
+    
+
+}
