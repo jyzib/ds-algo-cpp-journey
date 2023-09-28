5c7742df1362fccf82d57f92f3410cdaec70e518
jyzib <jazibzaidi02@gmail.com>
2023-09-28 08:12:34 +0530

    Day-3 Pattern question started

---
+++ day3.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+// find factoria number
+
+// int main(){
+//   int a;
+//   cout<<"Enter your number"<<endl;
+//   cin>>a;
+//   double fact = 1;
+//   for(int i = 1;i<=a;i++){
+//     fact = fact*i;
+//   }
+//   cout<<"Factorial of "<<a<<" is "<< fact<<endl;
+
+
+// }
+
+// find prime number
+
+// int main(){
+//     int a ;
+//     cin>>a;
+//     cout<<a%2<<endl;
+//     if(a<2){
+//         cout<<a<<" is a not prime number"<<endl;  
+//         return 0;
+//     }else{
+//     for(int i = 2;i<a;i++){
+
+//         if(a%i == 0){
+//             cout<<a<<" is a not prime number"<<endl;
+//              return 0;
+
+            
+//         }
+//     }
+
+//      cout<<a<<" Is a prime number"<<endl;
+//      return 0;
+//     }
+    
+// }
+
+
+// find fibucani number
+
+
+// int main(){
+//     int a;
+//     cin>>a;
+//     int current = 1;
+//     int prevous = 0;
+ 
+//      int cur;
+//     for(int i = 0;i<=a;i++){
+//        cur = current + prevous;
+//        cout<< cur<<endl;
+//         current =  prevous;
+        
+//  prevous = cur;
+//     }
+// }
+
+
+
+// print start pattern
+int main(){
+    for(int i = 0;i<5;i++){
+        for(int j = 0;j<5;j++){
+        cout<<'*';
+        }
+        cout<<endl;
+        
+
+    }
+}
