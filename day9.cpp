c24f621cad52b0343358144ad0424d47ef8a9b76
jyzib <jazibzaidi02@gmail.com>
2023-10-09 07:50:01 +0530

    Arithmetic Operator cpp

---
+++ day9.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+//     int a = 10;
+//     int b = --a;
+//   cout<<a<<endl<<b;
+
+//   comprasion oprator
+// int a = 3>4;
+//   cout<<a;
+//   logical operator
+// char name = 'a';
+// cin>>name;
+// if(name=='a'||name=='e'||name=='i'||name=='o'||name=='u'){
+//     cout<<"yes"<<name;
+// }else{
+//     cout<<"No";
+// }
+
+// bitwise operator
+
+int a = 4<<2;
+// left shift
+cout<<a<<endl;
+int b = 4>>2;
+// right shift
+cout<<b<<endl;
+// compliment
+
+int h = ~-6;
+cout<<h;
+}
