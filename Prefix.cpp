3f825ca0d076dbdb4de229e80a6d2de398c022a2
jyzib <jazibzaidi02@gmail.com>
2023-12-03 22:09:56 +0530

    Array question added

--- Prefix.cpp
+++ Prefix.cpp
@@ -21,26 +21,60 @@ using namespace std;
 // cout<<"false";
 //  return 0;
 // }
-int main(){
-    int arr[] = {4,2,0,5,2,6,2,3};
-    int wall = arr[0];
-    int unit = 0;
-    for(int i =1;i<7;i++){
-    int max = 0;
-        for(int j = i;j<=7;j++){
-            if(max < arr[j]){
-                max = arr[j];
-            }
-        }
-        if(wall < max){
-            if(wall > arr[i]){
-            unit += (wall - arr[i]);
-            }else{
-                wall = arr[i];
-            }
-        }else{
-            unit += (max - arr[i]);
-        }
-    }
-    cout<<unit<<endl;
-}
+// int main(){
+//     int arr[] = {4,2,0,5,2,6,2,3};
+//     int wall = arr[0];
+//     int unit = 0;
+//     for(int i =1;i<7;i++){
+//     int max = 0;
+//         for(int j = i;j<=7;j++){
+//             if(max < arr[j]){
+//                 max = arr[j];
+//             }
+//         }
+//         if(wall < max){
+//             if(wall > arr[i]){
+//             unit += (wall - arr[i]);
+//             }else{
+//                 wall = arr[i];
+//             }
+//         }else{
+//             unit += (max - arr[i]);
+//         }
+//     }
+//     cout<<unit<<endl;
+// }
+
+// int main(){
+//     int arr[] = {1,4,6,8,10,45};
+//     int target = 21;
+//     int val = false;
+//     for(int i = 0;i<=5-2;i++){
+//         for(int j = i+1;j<=5-1;j++){
+//             int ans = target - (arr[i] + arr[j]) ;
+//             cout<<ans<<endl;
+//             int start = j+1;
+//             int end = 5;
+//             while(start<end){
+//               int mid = (start + end) /2;
+//               if(arr[mid] == ans){
+              
+//                 val = true;
+//                 break;
+//               }else if(arr[mid] > target){
+//                 end = mid -1;
+//               }else{
+//                 start = mid +1;
+//               }
+//             }
+//             if(val){
+//                 break;
+//             }
+//         }
+//           if(val){
+//                 break;
+//             }
+      
+//     }
+//     cout<<val<<endl;
+// }
